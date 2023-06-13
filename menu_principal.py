41bc084883785e17d9bf7015f3c2ed579115de6c
Corsi <128002546+ECorsi@users.noreply.github.com>
2023-06-12 21:08:04 -0300

    menu_principal.py

--- menu_principal.py
+++ menu_principal.py
@@ -30,16 +30,16 @@ janela.config(bg=background)
 janela.resizable(width=False, height=False)
 
 # Icone da janela
-janela.iconbitmap(r"G:\Meu Drive\Python\PI\Imagens\logoprojetom.ico")
+janela.iconbitmap("Imagens/logoprojetom.ico")
 
 # Fundo da janela
 frame=Frame(janela,bg="red")
 frame.pack(fill="y")
-imagem_fundo=PhotoImage(file=r"G:\Meu Drive\Python\PI\Imagens\menu.png")
+imagem_fundo=PhotoImage(file="Imagens/menu.png")
 Label(frame,image=imagem_fundo).pack()
 
 # Botão Leaderboard
-rank_img=PhotoImage(file=r"G:\Meu Drive\Python\PI\Imagens\rank.png")
+rank_img=PhotoImage(file="Imagens/rank.png")
 largura = rank_img.width() // 3
 altura = rank_img.height() // 2
 rank_img = rank_img.subsample(3)  # Reduzir pela metade
@@ -47,12 +47,12 @@ botao_rank=Button(janela, image=rank_img, bg="white", bd=0, command=rank)
 botao_rank.place(x=150, y=270)
 
 # Botão JOGAR
-jogar_img=PhotoImage(file=r"G:\Meu Drive\Python\PI\Imagens\jogar.png")
+jogar_img=PhotoImage(file="Imagens/jogar.png")
 botao_jogar=Button(janela, image=jogar_img, bg="white", bd=0, command=jogar)
 botao_jogar.place(x=527, y=368)
 
 # botao Sair do jogo e voltar pra tela de login
-sair_img=PhotoImage(file=r"G:\Meu Drive\Python\PI\Imagens\sair.png")
+sair_img=PhotoImage(file="Imagens/sair.png")
 botao_sair=Button(janela, image=sair_img, bg="white", bd=0, command=sair)
 botao_sair.place(x=962, y=290)
 
